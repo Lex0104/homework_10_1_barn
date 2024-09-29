47f0be6cc57d3139c932d7f48777080858208f61
Eduard <barned85@gmail.com>
2024-09-30 02:46:24 +0300

    update module main.py

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from src.widget import mask_account_card, get_data
+from src.processing import filter_by_state, sort_by_date
+from src.widget import get_data, mask_account_card
 
-print(mask_account_card("MasterCard 7158300734726758"))
+print(mask_account_card("MasterCard  35383033474447895560"))
 
-print(mask_account_card("Счет 35383033474447895560"))
+print(mask_account_card("Счет 7158300734726758"))
 
 print(get_data("2024-03-11T02:26:18.671407"))
+
+print(
+    filter_by_state(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        ]
+    )
+)
+
+print(
+    sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        ]
+    )
+)
---
+++ src/processing.py
@@ -0,0 +1,23 @@
+def filter_by_state(list_of_dict: list, state: str = "EXECUTED") -> list:
+    """Функция возвращает новый список словарей, у которых ключ state соответствует указанному значению"""
+    filtred_list = []
+    for ld in list_of_dict:
+        if ld.get("state") == state:
+            filtred_list.append(ld)
+    return filtred_list
+
+
+def sort_by_date(list_of_dict: list, direction: bool = True) -> list:
+    """Принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание"""
+    sorted_list = sorted(list_of_dict, key=lambda x: ["date"], reverse=direction)
+    return sorted_list
+
+
+print(
+    sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        ]
+    )
+)
