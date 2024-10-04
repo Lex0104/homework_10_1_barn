0ef3b877f2dc2e756e9d35dbcf35991c28acfca2
p <barned85@gmail.com>
2024-10-05 02:21:29 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,3 +32,45 @@ def user_card_number() :
         "8990 92** **** 5229",
         "5999 41** **** 6353",
     ]
+
+
+import pytest
+
+@pytest.fixture
+def number_string() :
+    return [
+        "Maestro 1596 83** **** 5199",
+        "**9589",
+        "MasterCard 7158 30** **** 6758",
+        "**5560",
+        "Visa Classic 6831 98** **** 7658",
+        "Visa Platinum 8990 92** **** 5229",
+        "Visa Gold 5999 41** **** 6353",
+        "**4305",
+    ]
+
+
+@pytest.fixture
+def old_data():
+    return [
+        "11.07.2018",
+        "03.07.2019",
+        "30.06.2018",
+    ]
+
+import pytest
+
+
+@pytest.fixture
+def list_dict():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+@pytest.fixture
+def list_dict():
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
