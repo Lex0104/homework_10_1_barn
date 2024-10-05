9dd488b6cb4c4993592943ef33589a8c5055fa58
p <barned85@gmail.com>
2024-10-06 01:36:38 +0300

    Update test_processing.py
    'get_sort_list_date' has changed "sort_by_date"

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -17,16 +17,23 @@ def test_filter_by_state():
             (
                     [
                         {
-                            "id" : 41428829,
-                            "state" : "EXECUTED",
-                            "date" : "2019-07-03T18:35:29.512364",
+                            "id": 41428829,
+                            "state": "EXECUTED",
+                            "date": "2019-07-03T18:35:29.512364",
                         },
                         {
-                            "id" : 939719570,
-                            "state" : "EXECUTED",
-                            "date" : "2018-06-30T02:08:58.425572",
+                            "id": 939719570,
+                            "state": "EXECUTED",
+                            "date": "2018-06-30T02:08:58.425572",
                         }
+                    ]
+            )
+        ]
+    )
+)
 
 
-def test_get_sort_list_date(list_dict, direction, sorted_list):
+
+def test_sort_by_date(list_dict, direction, sorted_list):
+    assert get_sort_by_date(list_dict, direction) == sorted_list
     assert get_sort_list_date(list_dict, direction) == sorted_list
