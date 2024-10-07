79fa3a632adaa3e33c97aa46dfb7049a168b0e79
p <barned85@gmail.com>
2024-10-07 12:14:35 +0300

    Update test_processing.py

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -7,28 +7,27 @@ def test_filter_by_state(list_of_dict):
         {"date": "2019-07-03T18:35:29.512364", "id": 41428829, "state": "EXECUTED"},
         {"date": "2018-06-30T02:08:58.425572", "id": 939719570, "state": "EXECUTED"},
     ]
-
-def test_filter_by_state():
-    assert filter_by_state ( [] ) == []
-
-@pytest.mark.parametrize(
+    
+    
+    
+ @pytest.mark.parametrize(
     "list_dict, direction, sorted_list"
-        [
-            (
-                    [
-                        {
-                            "id" : 41428829,
-                            "state" : "EXECUTED",
-                            "date" : "2019-07-03T18:35:29.512364",
-                        },
-                        {
-                            "id" : 939719570,
-                            "state" : "EXECUTED",
-                            "date" : "2018-06-30T02:08:58.425572",
-                        }
-                    ]
-            )
-        ],
+     [
+         (
+                 [
+                     {
+                        "id" : 41428829,
+                        "state" : "EXECUTED",
+                         "date" : "2019-07-03T18:35:29.512364",
+                     },
+                     {
+                         "id" : 939719570,
+                         "state" : "EXECUTED",
+                         "date" : "2018-06-30T02:08:58.425572",
+                     }
+                 ]
+          )
+    ],
 )
 
 
