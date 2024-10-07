afbdee3281b59707fbad4b9276b26beacab2ae33
p <barned85@gmail.com>
2024-10-07 11:53:56 +0300

    Update conftest.py
    change all

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,43 +1,7 @@
-import pytest
+mport pytest
 
 @pytest.fixture
-def number_string() :
-    return [
-        "Maestro 1596 83** **** 5199",
-        "**9589",
-        "MasterCard 7158 30** **** 6758",
-        "**5560",
-        "Visa Classic 6831 98** **** 7658",
-        "Visa Platinum 8990 92** **** 5229",
-        "Visa Gold 5999 41** **** 6353",
-        "**4305",
-    ]
-
-
-@pytest.fixture
-def old_data() :
-    return [
-        "11.07.2018",
-        "03.07.2019",
-        "30.06.2018",
-    ]
-
-
-@pytest.fixture
-def user_card_number() :
-    return [
-        "7000 79** **** 6361",
-        "7158 30** **** 6758",
-        "6831 98** **** 7658",
-        "8990 92** **** 5229",
-        "5999 41** **** 6353",
-    ]
-
-
-import pytest
-
-@pytest.fixture
-def number_string() :
+def number_string():
     return [
         "Maestro 1596 83** **** 5199",
         "**9589",
@@ -58,8 +22,26 @@ def old_data():
         "30.06.2018",
     ]
 
-import pytest
 
+@pytest.fixture
+def user_card_number():
+    return [
+        "7000 79** **** 6361",
+        "7158 30** **** 6758",
+        "6831 98** **** 7658",
+        "8990 92** **** 5229",
+        "5999 41** **** 6353",
+    ]
+
+@pytest.fixture
+def account_number():
+    return [
+        "**4305",
+        "**9589",
+        "**5560",
+        "**4305",
+    ]
+import pytest
 
 @pytest.fixture
 def list_dict():
