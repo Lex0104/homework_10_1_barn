216273e725150278a4479d9277686870beb3ea85
p <barned85@gmail.com>
2024-10-07 22:13:19 +0300

    Update test_masks.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,20 +2,21 @@ import pytest
 
 from src.masks import get_mask_card_number, get_mask_account
 
+
 @pytest.mark.parametrize(
     "user_card_number, new_string", [
-        ('7000792289606361', '7000 79** **** 6361'),
-        ('7158300734726758", "7158 30** **** 6758'),
-        ('6831982476737658", "6831 98** **** 7658'),
-        ('8990922113665229", "8990 92** **** 5229'),
-        ('5999414228426353", "5999 41** **** 6353'),
+        ("7000792289606361", "7000 79** **** 6361"),
+        ("7158300734726758", "7158 30** **** 6758"),
+        ("6831982476737658", "6831 98** **** 7658"),
+        ("8990922113665229", "8990 92** **** 5229"),
+        ("5999414228426353", "5999 41** **** 6353"),
     ]
 )
 def test_get_mask_card_number(user_card_number, new_string):
     assert get_mask_card_number(user_card_number) == new_string
 
 
-@pytest.mark.parametrize(
+@pytest.mark.parametrize (
     "account_number, new_mask",
     [
         ("73654108430135874305", "**4305"),
