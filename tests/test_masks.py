e6735bf5eb02f5a600bda8f6c8c7c36102ff9d23
p <barned85@gmail.com>
2024-10-07 11:14:02 +0300

    Update test_masks.py
    update parametrize

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,23 +2,21 @@ import pytest
 
 from src.masks import get_mask_card_number, get_mask_account
 
-
 @pytest.mark.parametrize(
-    "user_card_number, return",
-    [
-        ("7000792289606361", "7000 79** **** 6361"),
-        ("7158300734726758", "7158 30** **** 6758"),
-        ("6831982476737658", "6831 98** **** 7658"),
-        ("8990922113665229", "8990 92** **** 5229"),
-        ("5999414228426353", "5999 41** **** 6353"),
-    ],
+    "user_card_number, new_string", [
+        ('7000792289606361', '7000 79** **** 6361'),
+        ('7158300734726758", "7158 30** **** 6758'),
+        ('6831982476737658", "6831 98** **** 7658'),
+        ('8990922113665229", "8990 92** **** 5229'),
+        ('5999414228426353", "5999 41** **** 6353'),
+    ]
 )
-def test_get_mask_card_number(user_card_number):
-    assert get_mask_card_number(user_card_number)
+def test_get_mask_card_number(user_card_number, new_string):
+    assert get_mask_card_number(user_card_number) == new_string
 
 
 @pytest.mark.parametrize(
-    "user_card_number, return",
+    "account_number, new_mask",
     [
         ("73654108430135874305", "**4305"),
         ("64686473678894779589", "**9589"),
@@ -26,5 +24,5 @@ def test_get_mask_card_number(user_card_number):
         ("73654108430135874305", "**4305"),
     ],
 )
-def test_get_mask_account(user_card_number):
-    assert get_mask_account(user_card_number) == return
+def test_get_mask_account(account_number, new_mask):
+    assert get_mask_account(account_number) == new_mask
