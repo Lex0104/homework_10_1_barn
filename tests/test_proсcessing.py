5dbdd7c36b8d18ada2d4d593d038911db6408295
p <barned85@gmail.com>
2024-10-07 22:14:12 +0300

    Update test_proсcessing.py

--- tests/test_proсcessing.py
+++ tests/test_proсcessing.py
@@ -1,35 +1,27 @@
 import pytest
 
-from src.processing import filter_by_state, sort_by_date
+from src.proccessing import filter_by_state, sort_by_date
 
-def test_filter_by_state(list_of_dict):
-    assert filter_by_state(list_of_dict) == [
+def test_filter_by_state(list_dict):
+    assert filter_by_state(list_dict) == [
         {"date": "2019-07-03T18:35:29.512364", "id": 41428829, "state": "EXECUTED"},
         {"date": "2018-06-30T02:08:58.425572", "id": 939719570, "state": "EXECUTED"},
     ]
-    
-    
-    
- @pytest.mark.parametrize(
-    "list_dict, direction, sorted_list"
-     [
-         (
-                 [
-                     {
-                        "id" : 41428829,
-                        "state" : "EXECUTED",
-                         "date" : "2019-07-03T18:35:29.512364",
-                     },
-                     {
-                         "id" : 939719570,
-                         "state" : "EXECUTED",
-                         "date" : "2018-06-30T02:08:58.425572",
-                     }
-                 ]
-          )
-    ],
-)
 
+@pytest.mark.parametrize("list_dict, sorted_list", [
+    (
+            [
+                {"id" : 41428829,
+                 "state" : "EXECUTED",
+                 "date" : "2019-07-03T18:35:29.512364"
+                 },
+                 {"id" : 939719570,
+                  "state" : "EXECUTED",
+                  "date" : "2018-06-30T02:08:58.425572"}
+                  ]
+    )
+]
+                         )
 
-def test_sort_by_date(list_dict, direction, sorted_list):
-    assert sort_by_date(list_dict, direction) == sorted_list
+def test_sort_by_date(list_dict, sorted_list):
+    assert sort_by_date(list_dict) == sorted_list
