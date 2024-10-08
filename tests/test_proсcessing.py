480dd3ad8971f1f42bc10335fe77d93d148045a8
p <barned85@gmail.com>
2024-10-08 18:56:05 +0300

    Update test_proсcessing.py
    'test_sort_by_date' update

--- tests/test_proсcessing.py
+++ tests/test_proсcessing.py
@@ -8,20 +8,9 @@ def test_filter_by_state(list_dict):
         {"date": "2018-06-30T02:08:58.425572", "id": 939719570, "state": "EXECUTED"},
     ]
 
-@pytest.mark.parametrize("list_dict, sorted_list", [
-    (
-            [
-                {"id" : 41428829,
-                 "state" : "EXECUTED",
-                 "date" : "2019-07-03T18:35:29.512364"
-                 },
-                 {"id" : 939719570,
-                  "state" : "EXECUTED",
-                  "date" : "2018-06-30T02:08:58.425572"}
-                  ]
-    )
-]
-                         )
 
-def test_sort_by_date(list_dict, sorted_list):
-    assert sort_by_date(list_dict) == sorted_list
+def test_sort_by_date(list_dict):
+    assert sort_by_date(list_dict) == [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+    ]
