2fe718508789b45040292d8868e95ef19f655a30
Eduard <barned85@gmail.com>
2024-10-22 15:05:00 +0300

    new main

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -3,4 +3,24 @@ from src.external_api import converter
 import requests
 
 
-@patch()
+@patch('requests.get')
+def test_converter(mock_get):
+    mock_get.return_value.json.return_value = {
+   "date": "2024-10-22",
+    "id": "587085106",
+    "name": "доллар",
+    "code": "USD"
+}
+
+    assert converter == {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+        }
+    }
+}
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,9 @@
 import json
 import requests
-from unittest.mock import patch
-from utils import get_financial_transactions_data
+from unittest.mock import MagicMock,patch
+from src.utils import get_financial_transactions_data
 
-def get_financial_transactions_data(empty_list):
-
+@patch('transcations')
+def get_financial_transactions_data(mock_open: MagicMock, path_name:patch, mock_file=None) -> None:
+    mock_file.read.return_value = json.dumps([{"id": 441945886}])
+    assert get_financial_transactions_data(path_name) == ([{"id": 441945886}])
