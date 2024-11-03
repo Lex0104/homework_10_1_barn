c3f8e1eb946105d20d2e017d94b82c92c214eb19
Eduard <barned85@gmail.com>
2024-11-04 02:17:10 +0300

    add function on the module main.
    add src/import

--- main.py
+++ main.py
@@ -4,6 +4,9 @@
 import os
 from src.utils import get_financial_transactions_data
 from src.transactions_csv_excel import transactions_csv, transactions_xlsx
+from src.proccessing import sort_by_date, filter_by_state
+from src.dictionary_search import dict_search_bank
+from src.widget import mask_account_card, get_data
 
 
 transactions = [
@@ -55,7 +58,7 @@ transactions = [
 ]
 
 
-def main():
+def main_1():
     work_file = input(
         "Привет! Добро пожаловать в программу работы с банковскими транзакциями. "
         "\nВыберите необходимый пункт меню: "
@@ -80,43 +83,129 @@ def main():
         else:
             input("Данного варианта нет в списке, попробуйте еще раз:\nВвод: ")
 
-            status_operation = (
-                input(
-                    "\nВведите статус, по которому необходимо выполнить фильтрацию. "
-                    "\nДоступные для фильтровки статусы: "
-                    "EXECUTED, CANCELED, PENDING:\nВвод: "
-                )
-                .strip()
-                .upper()
-            )
+            # status_operation = (
+            #     input(
+            #         "\nВведите статус, по которому необходимо выполнить фильтрацию. "
+            #         "\nДоступные для фильтровки статусы: "
+            #         "EXECUTED, CANCELED, PENDING:\nВвод: "
+            #     )
+            #     .strip()
+            #     .upper()
+            # )
+            #
+            # while True:
+            #     if status_operation == "PENDING" or status_operation == "EXECUTED" or status_operation == "CANCELED":
+            #         status_operation = (
+            #             input(
+            #                 f"Статус {status_operation} не доступен.\n"
+            #                 "\nВведите статус, по которому необходимо выполнить фильтрацию."
+            #                 "\nДоступные для фильтровки статусы: "
+            #                 "EXECUTED, CANCELED, PENDING:\nВвод: "
+            #             )
+            #             .strip()
+            #             .upper()
+            #         )
 
-            while True:
-                if status_operation == "PENDING" or status_operation == "EXECUTED" or status_operation == "CANCELED":
-                    status_operation = (
-                        input(
-                            f"Статус {status_operation} не доступен.\n"
-                            "\nВведите статус, по которому необходимо выполнить фильтрацию."
-                            "\nДоступные для фильтровки статусы: "
-                            "EXECUTED, CANCELED, PENDING:\nВвод: "
-                        )
-                        .strip()
-                        .upper()
-                    )
 
-        print("Отсортировать операции по дате? Да/Нет")
-        user_client = input("Введите да или нет ").lower()
-        if user_client == "Да":
-            print("Отсортировать по возростанию или по убыванию?")
-        user_client_down = input("В порядке убывания/ В порядке возрастания").lower()
-        if user_client_down == " В порядке возрастания":
-            print("Выводить только рублевые транзакции? Да/Нет")
-        user_like_rub = input("Введите да или нет ").lower()
-        if user_like_rub == "Да":
-            print("Отфильтровать список транзакций по определенному слову в описании? Да/Нет")
-        user_transactions = input("Введите да или нет ").lower()
-        if user_transactions == "Да":
-            print("Распечатываю итоговый список транзакций...")
-        result(data)
+def choice_state(data: list) -> list:
+    """Фильтрация данных по выбранному статусу"""
+    while True:
+        print(
+            "Введите статус, по которому необходимо выполнить фильтрацию."
+            "Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING"
+        )
+        user_input_2 = input()
+        if user_input_2.upper() != "EXECUTED" and user_input_2.upper() != "CANCELED" and user_input_2.upper() != "PENDING":
+            print(f'Статус операции "{user_input_2}" недоступен')
+        else:
+            print(f'Операции отфильтрованы по статусу "{user_input_2}"')
+            data = filter_by_state(data, user_input_2.upper())
+            break
+    return data
+
+
+def choice_sort_date(data: list) -> list:
+    """Сортировка по дате"""
+    user_sort = input()
+    if user_sort.lower() == "да":
+        print("Отсортировать по возрастанию или по убыванию?")
+        sort_up_or_lower = input()
+        if sort_up_or_lower.lower() == "по возрастанию":
+            is_reverse = False
+            data = sort_by_date(data, is_reverse)
+        else:
+            is_reverse = True
+            data = sort_by_date(data, is_reverse)
+        return data
+
+
+def sort_rub(data: list) -> list:
+    """Фильтрация по рублевым транзакциям"""
+    rub_transaction = input()
+    if rub_transaction.lower() == "да":
+        data = filter_by_currency(data, "RUB")
+    return list(data)
+
+
+def sort_word(data: list, sort_by_word: str) -> list:
+    """Фильтрация по строке"""
+    if sort_by_word.lower() == "да":
+        print("Введите слово:")
+        string_to_search = input()
+        data = dict_search_bank(data, string_to_search)
+    return data
+
+
+def final_result(data: list) -> None:
+    """Вычисление и вывод результатов по полученному списку транзакций"""
+    if len(data) == 0:
+        print("Не найдено ни одной транзакции, подходящей под ваши условия фильтрации")
+    else:
+        print(f"Всего банковских операций в выборке: {len(data)}\n")
+
+        for transaction in data:
+            date = get_data(transaction.get("date"))
+
+            try:
+                mask_from = mask_account_card(transaction["from"])
+                print(f"{date} {transaction} {mask_from} -> ", end="")
+            except KeyError:
+                print(f"{date} {transaction} ", end="")
+            except AttributeError:
+                print(f"{date} {transaction}", end="")
+
+                mask_to = mask_account_card(transaction["to"])
+            try:
+                amount = transaction["amount"]
+            except KeyError:
+                amount = transaction["operationAmount"]["amount"]
+            try:
+                currency = transaction["currency_name"]
+            except KeyError:
+                currency = transaction["operationAmount"]["currency"]["name"]
+            print(f"{mask_to} Сумма: {amount} {currency}")
+
+
+def main() -> None:
+    """Возвращает список транзакций по выбранным условиям"""
+    print(
+        """Привет! Добро пожаловать в программу работы с банковскими транзакциями.
+    Выберите необходимый пункт меню:
+    1. Получить информацию о транзакциях из JSON-файла
+    2. Получить информацию о транзакциях из CSV-файла
+    3. Получить информацию о транзакциях из XLSX-файла"""
+    )
+    data = main_1()
+    data = choice_state(data)
+    print("Отсортировать операции по дате? Да/Нет")
+    data = choice_sort_date(data)
+    print("Выводить только рублевые транзакции? Да/Нет")
+    data = sort_rub(data)
+    print("Отфильтровать список транзакций по определенному слову в описании? Да/Нет")
+    sort_by_word = input()
+    data = sort_word(data, sort_by_word)
+    print("Распечатываю итоговый список транзакций...")
+    final_result(data)
 
 
 if __name__ == "__main__":
